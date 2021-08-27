927b513511e887d96bd813dae0b4c87f021d0f2e
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-08-27 08:03:51 +0300

    feat(layout): add the public directory & robots

--- config/paths.ts
+++ config/paths.ts
@@ -1,6 +1,8 @@
 import { join } from "path";
 
 export const rootDir = join(__dirname, "..");
+export const publicDir = join(rootDir, "public");
 export const serverDir = join(rootDir, "server");
+export const srcDir = join(rootDir, "src");
 
 export const srcDbFile = join(serverDir, "db.json");
