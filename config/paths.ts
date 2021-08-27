ef9558022b08ac646a227032394f1da259d2f2bf
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-08-27 09:48:25 +0300

    feat(config): add some paths for the future

--- config/paths.ts
+++ config/paths.ts
@@ -1,8 +1,14 @@
 import { join } from "path";
 
 export const rootDir = join(__dirname, "..");
+export const buildDir = join(rootDir, "build");
+export const outDir = join(rootDir, "out");
 export const publicDir = join(rootDir, "public");
 export const serverDir = join(rootDir, "server");
 export const srcDir = join(rootDir, "src");
 
+export const outDocDir = join(outDir, "doc");
+
 export const srcDBFile = join(serverDir, "db.json");
+
+export const srcFiles = join(srcDir, "**", "*.ts(|x)");
