9737f96cf0119258326914bdc6e554f679f08a0e
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-08-27 20:10:20 +0300

    refactor(layout): move the build dir inside the output one

--- config/paths.ts
+++ config/paths.ts
@@ -1,12 +1,12 @@
 import { join } from "path";
 
 export const rootDir = join(__dirname, "..");
-export const buildDir = join(rootDir, "build");
 export const outDir = join(rootDir, "out");
 export const publicDir = join(rootDir, "public");
 export const serverDir = join(rootDir, "server");
 export const srcDir = join(rootDir, "src");
 
+export const outBuildDir = join(outDir, "build");
 export const outDocDir = join(outDir, "doc");
 
 export const srcDBFile = join(serverDir, "db.json");
