6ef2514c9df1f951fbaeec7b0b9a074b980ce36b
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-08-28 04:03:12 +0300

    chore(docs): add the out-of-file path list

--- config/paths.ts
+++ config/paths.ts
@@ -1,5 +1,16 @@
 import { join } from "path";
 
+// In a case of a path change, please consider that even though this is the
+// main path defintion file, it is not the only place where they are stored.
+// Path definitions outside of this file include:
+// -   All the test paths (both for input and output). They are defined in the
+//     cypress.json file in the root directory.
+// -   The relative ES import paths. They must be string literals, so they are
+//     hardcoded. However, most can be found by searching for the `".` pattern.
+// -   The ignored paths in the `.*ignore` files and `tsconfig.json`.
+// -   The Yarn paths in `.vim/coc-settings.json`, `.vscode/settings.json` and
+//     `.yarnrc.yml`.
+
 export const rootDir = join(__dirname, "..");
 export const outDir = join(rootDir, "out");
 export const publicDir = join(rootDir, "public");
