e2687f8828df871599edb43a1158b20a3f93523a
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-08-28 14:20:28 +0300

    chore(config): minor test plugin config changes

--- test/plugins/index.ts
+++ test/plugins/index.ts
@@ -1,12 +1,13 @@
 import { startDevServer } from "@cypress/webpack-dev-server";
+
 // @ts-ignore TS7016
-import codeCoverage from "@cypress/code-coverage/task";
+import codeCoveragePlugin from "@cypress/code-coverage/task";
 import "cypress";
 
 import webpackFunction from "../../webpack.config";
 
 export const pluginConfig: Cypress.PluginConfig = (on, config) => {
-    codeCoverage(on, config);
+    codeCoveragePlugin(on, config);
 
     // The `load-webpack` plugin does not seem to support
     // TypeScript config, so we have to load the webpack
@@ -15,7 +16,7 @@ export const pluginConfig: Cypress.PluginConfig = (on, config) => {
 
     const webpackConfig = webpackFunction({ WEBPACK_SERVE: "true" });
 
-    on("dev-server:start", async (options) => {
+    on("dev-server:start", (options) => {
         return startDevServer({ options, webpackConfig });
     });
 
