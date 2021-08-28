4b48195921aa4360c604ef79866026962f4d3d55
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-08-28 13:44:47 +0300

    test(deps): add the React and code coverage plugins

--- test/plugins/index.ts
+++ test/plugins/index.ts
@@ -1,5 +1,23 @@
+import { startDevServer } from "@cypress/webpack-dev-server";
+// @ts-ignore TS7016
+import codeCoverage from "@cypress/code-coverage/task";
 import "cypress";
 
-export const pluginConfig: Cypress.PluginConfig = (_on, _config) => {};
+import webpackFunction from "../../webpack.config";
+
+export const pluginConfig: Cypress.PluginConfig = (on, config) => {
+    codeCoverage(on, config);
+
+    // The `load-webpack` plugin does not seem to support
+    // TypeScript config, so we have to load the webpack
+    // config directly and set the environment manually.
+    config.env.reactDevtools = true;
+
+    const webpackConfig = webpackFunction({ WEBPACK_SERVE: "true" });
+
+    on("dev-server:start", async (options) => {
+        return startDevServer({ options, webpackConfig });
+    });
+};
 
 export default pluginConfig;
