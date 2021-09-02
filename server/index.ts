cfb44f889e717ba7ab4d107c14de019bb4d558cc
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-09-02 18:33:04 +0300

    chore(misc): do some minor text changes

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,6 @@ import jsonServer from "json-server";
 const port = Number(process.env.PORT === undefined ? 3031 : process.env.PORT);
 const server = jsonServer.create();
 
-server.get("/", (_, response) => response.send("GET / now has status OK"));
+server.get("/", (_, response) => response.send("GET / now has status 200"));
 server.use(jsonServer.router(join(__dirname, "db.json")));
 server.listen(port, () => console.log(`json server: started (port ${port})`));
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
 import "./Home.scss";
 
 /** The home page. */
-export const Home: React.FC<{}> = () => <div>The home page</div>;
+export const Home: React.FC<{}> = () => (
+    <div>This is the home page. Nothing to see here yet.</div>
+);
 
 export default Home;
