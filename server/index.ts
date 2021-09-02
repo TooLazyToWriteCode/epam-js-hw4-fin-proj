f7928bec91fb9f8d4553e266ae160ea2902c3b51
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-09-02 21:56:31 +0300

    feat(server): make server serve the static files

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,11 @@
 import { join } from "path";
+import express from "express";
 import jsonServer from "json-server";
 
 const port = Number(process.env.PORT === undefined ? 3031 : process.env.PORT);
 const server = jsonServer.create();
 
 server.get("/", (_, response) => response.send("GET / now has status 200"));
+server.use("/images", express.static(join(__dirname, "images")));
 server.use(jsonServer.router(join(__dirname, "db.json")));
 server.listen(port, () => console.log(`json server: started (port ${port})`));
