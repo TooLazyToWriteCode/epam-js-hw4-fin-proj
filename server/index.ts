c326a9089aab9ee43e573c7f59234370fee6f3f5
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-09-03 12:00:49 +0300

    chore(scripts): should be fine after the port is opened

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,6 @@ import jsonServer from "json-server";
 const port = process.env.PORT === undefined ? 3333 : Number(process.env.PORT);
 const server = jsonServer.create();
 
-server.get("/", (_, response) => response.send("GET / now has status 200"));
 server.use("/images", express.static(join(__dirname, "images")));
 server.use(jsonServer.router(join(__dirname, "db.json")));
 server.listen(port, () => console.log(`json server: started (port ${port})`));
