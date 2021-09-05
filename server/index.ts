1ef773f9583960fe745e95427fae904cd5406846
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-09-05 20:10:17 +0300

    chore(server): rewrite the imports and the structure a bit

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import jsonServer from "json-server";
+import { static as staticDir } from "express";
+import { create, router } from "json-server";
 import { join } from "path";
 
-const port = process.env.PORT === undefined ? 3333 : Number(process.env.PORT);
-const server = jsonServer.create();
+const port: number = Number(process.env.PORT) || 3333;
 
-server.use("/images", express.static(join(__dirname, "images")));
-server.use(jsonServer.router(join(__dirname, "db.json")));
-server.listen(port, () => console.log(`json server: started (port ${port})`));
+create()
+    .use("/images", staticDir(join(__dirname, "images")))
+    .use(router(join(__dirname, "db.json")))
+    .listen(port, () => console.log(`json server: started (port ${port})`));
