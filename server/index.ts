98cac18d573abbeef9ee59cc4ab92f93fc990af3
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-09-05 23:38:34 +0300

    fix(server): use in-memory database, do not write to disk

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,12 @@ import { static as staticDir } from "express";
 import { defaults, create, router } from "json-server";
 import { join } from "path";
 
+import database from "./db.json";
+
 const port: number = Number(process.env.PORT) || 3333;
 
 create()
     .use(defaults({ noCors: true }))
     .use("/images", staticDir(join(__dirname, "images")))
-    .use(router(join(__dirname, "db.json")))
+    .use(router(database))
     .listen(port, () => console.log(`started (port: ${port})`));
