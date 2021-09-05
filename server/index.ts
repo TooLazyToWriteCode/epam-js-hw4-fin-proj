94ba8c702a6db3dfb387c2ab2bb73de7bbe5a317
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-09-05 23:26:11 +0300

    chore(server): even better approach

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,11 @@
 import { static as staticDir } from "express";
-import { create, router } from "json-server";
+import { defaults, create, router } from "json-server";
 import { join } from "path";
 
 const port: number = Number(process.env.PORT) || 3333;
 
 create()
-    .use((_, response, next) => {
-        response.header("Access-Control-Allow-Headers", "*");
-        response.header("Access-Control-Allow-Origin", "*");
-        next();
-    })
+    .use(defaults({ noCors: true }))
     .use("/images", staticDir(join(__dirname, "images")))
     .use(router(join(__dirname, "db.json")))
     .listen(port, () => console.log(`started (port: ${port})`));
