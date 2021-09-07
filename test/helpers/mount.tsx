ed92a6114ebfa30105786d2e0653903b8d7cbf63
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-09-07 14:53:25 +0300

    chore(test): tests will need a rewrite too, remove them for now

--- test/helpers/mount.tsx
+++
@@ -1,6 +0,0 @@
-import { mount } from "@cypress/react";
-import { MemoryRouter } from "react-router-dom";
-
-export const mountPage = (url: string, node: React.ReactNode): void => {
-    mount(<MemoryRouter initialEntries={[url]}>{node}</MemoryRouter>);
-};
--- test/helpers/pages.ts
+++
@@ -1,9 +0,0 @@
-import pages, { Page } from "../../src/pages";
-
-export type PagesCallback = (name: string, page: Page) => void;
-
-export const goThroughPages = (callback: PagesCallback): void => {
-    Object.entries(pages).forEach(([name, page]: [string, Page]) => {
-        return callback(name, page);
-    });
-};
