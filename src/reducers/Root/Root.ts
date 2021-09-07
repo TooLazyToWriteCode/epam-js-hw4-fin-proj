2dadd54dc789e3a3b93e810662c046dace0aa262
TooLazyToWriteCode <72210675+TooLazyToWriteCode@users.noreply.github.com>
2021-09-07 18:22:09 +0300

    chore(config): also add the pokemons reducer to the loader

--- src/reducers/Root/Root.ts
+++ src/reducers/Root/Root.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import { pageTabsReducer } from "@/reducers/PageTabs";
+import { pokemonsReducer } from "@/reducers/Pokemons";
 
 export const rootReducer = combineReducers({
     pageTabs: pageTabsReducer,
+    pokemons: pokemonsReducer,
 });
